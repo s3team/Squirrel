7f131c0385700bf796d02310fafe7cda0b2c6e75
Yongheng Chen <changochen1@gmail.com>
2022-11-23 10:52:43 -0500

    Update run.sh

--- squirrel_srcs/SQLite/run.sh
+++ squirrel_srcs/SQLite/run.sh
@@ -1,1 +1,4 @@
+export AFL_CUSTOM_MUTATOR_ONLY=1
+export AFL_CUSTOM_MUTATOR_LIBRARY=`pwd`/custom_mutator.so
+export AFL_DISABLE_TRIM=1
 ../../afl-fuzz -i fuzz_root/input -H fuzz_root/init_lib -o output -- ./harness
