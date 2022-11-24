d02dba516930df2cbe96d64643628d26d3017fc0
Yongheng Chen <changochen1@gmail.com>
2022-11-23 22:52:07 -0500

    Fix cmake

--- squirrel_srcs/SQLite/clang-format.cmake
+++ squirrel_srcs/SQLite/clang-format.cmake
@@ -2,15 +2,11 @@
 
 # get all project files
 file(GLOB_RECURSE ALL_SOURCE_FILES srcs/* include/* src/*)
-#foreach(SOURCE_FILE ${ALL_SOURCE_FILES})
-#  string(FIND ${SOURCE_FILE} ${PROJECT_TRDPARTY_DIR} PROJECT_TRDPARTY_DIR_FOUND)
-#  if(NOT ${PROJECT_TRDPARTY_DIR_FOUND} EQUAL -1)
-#    list(REMOVE_ITEM ALL_SOURCE_FILES ${SOURCE_FILE})
-#  endif()
-#endforeach()
+# foreach(SOURCE_FILE ${ALL_SOURCE_FILES}) string(FIND ${SOURCE_FILE}
+# ${PROJECT_TRDPARTY_DIR} PROJECT_TRDPARTY_DIR_FOUND) if(NOT
+# ${PROJECT_TRDPARTY_DIR_FOUND} EQUAL -1) list(REMOVE_ITEM ALL_SOURCE_FILES
+# ${SOURCE_FILE}) endif() endforeach()
 
 set(clangformat_style "{BasedOnStyle: Google, IndentWidth: 2}")
-add_custom_target(
-  clangformat
-  ALL clang-format -style=${clangformat_style} -i
-          ${ALL_SOURCE_FILES})
+add_custom_target(clangformat ALL clang-format -style=${clangformat_style} -i
+                                  ${ALL_SOURCE_FILES})
