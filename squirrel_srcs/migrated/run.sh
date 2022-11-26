9892ab7843dfad786e7d941aa005a02db1f4ce90
Yongheng Chen <changochen1@gmail.com>
2022-11-25 23:31:38 -0500

    Fix run.sh

--- squirrel_srcs/migrated/run.sh
+++ squirrel_srcs/migrated/run.sh
@@ -1,8 +1,9 @@
 export AFL_CUSTOM_MUTATOR_ONLY=1
-export AFL_CUSTOM_MUTATOR_LIBRARY=`pwd`/build/libmysql_mutator.so
-#export AFL_CUSTOM_MUTATOR_LIBRARY=`pwd`/build/libsqlite_mutator.so
+#export AFL_CUSTOM_MUTATOR_LIBRARY=`pwd`/build/libmysql_mutator.so
+export AFL_CUSTOM_MUTATOR_LIBRARY=`pwd`/build/libsqlite_mutator.so
 export AFL_DISABLE_TRIM=1
 export AFL_AUTORESUME=1
 #export AFL_DEBUG=1
 #export AFL_NO_UI=1
-../../afl-fuzz -i fuzz_root/mysql_input -H ./config_test.yml -o output -S $1 -- ./build/db_driver ./config_test.yml
+../../afl-fuzz -i fuzz_root/input -H ./config.yml -o output -S $1 -- ./tests/test_bins/harness
+#../../afl-fuzz -i fuzz_root/mysql_input -H ./config_test.yml -o output -S $1 -- ./build/db_driver ./config_test.yml
